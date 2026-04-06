feb50425ee10d1d9122d29a0f287bbac619ac884
Srinivas Harish <srinivas@dockware.ai>
2026-04-05 23:47:00 -0400

    chore: wire up __init__.py exports, expand __all__

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,10 @@
-__all__ = []
+"""ALCNet: Attention-Layer Convolutional Network with FiLM Conditioning."""
+
+__all__ = [
+    "AlcnetCfg",
+    "GatedHybridClassifier",
+    "train_and_eval",
+]
+
+from .main import AlcnetCfg, GatedHybridClassifier, train_and_eval
 
